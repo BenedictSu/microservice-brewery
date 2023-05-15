6222da183c92387d62fe272b0fb18c489ea34d89
ben <BenedictTheGreat@hotmail.com>
2023-05-15 21:56:58 +0200

    init spring project

---
+++ src/main/java/com/brewery/microservicebrewery/MicroserviceBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.microservicebrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/microservicebrewery/MicroserviceBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.microservicebrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
